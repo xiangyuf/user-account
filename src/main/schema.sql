6a06af27c4f0c94788b840f37993d03a247a8bf0
xiangyu0xf <keven.feng@gmail.com>
2017-09-24 23:12:44 +0800

    Update schema.sql

--- src/main/schema.sql
+++ src/main/schema.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA `user_account` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `user_account`.`user` (
-  `uid` BIGINT NOT NULL,
+  `user_id` BIGINT NOT NULL,
   `coins` BIGINT DEFAULT 0,
-  PRIMARY KEY (`uid`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
